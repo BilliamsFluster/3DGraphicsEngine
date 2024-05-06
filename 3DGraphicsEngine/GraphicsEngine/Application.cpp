9bbed48fda59ad19bdafaa53edcfc3cdca05e075
William <50932749+BilliamsFluster@users.noreply.github.com>
2024-05-06 19:22:58 -0400

    Created index and constant buffer

--- 3DGraphicsEngine/GraphicsEngine/Application.cpp
+++ 3DGraphicsEngine/GraphicsEngine/Application.cpp
@@ -23,6 +23,6 @@ void Application::DoFrame()
 {
     const float c = sin(timer.Peek()) / 2 + 0.5f;
     wnd.Gfx().ClearBuffer(c, c, 1.0f);
-    wnd.Gfx().DrawTestTriangle();
+    wnd.Gfx().DrawTestTriangle(timer.Peek());
     wnd.Gfx().EndFrame();
 }
--- 3DGraphicsEngine/GraphicsEngine/Core/DirectX/Shaders/VertexShader.hlsl
+++ 3DGraphicsEngine/GraphicsEngine/Core/DirectX/Shaders/VertexShader.hlsl
@@ -1,4 +1,16 @@
-float4 main( float2 pos : POSITION ) : SV_POSITION
+struct VSOut
 {
-    return float4(pos.x, pos.y, 0.0f, 1.0f);
+    float3 color : COLOR;
+    float4 pos : SV_POSITION;
+};
+cbuffer CBuffer
+{
+    row_major matrix transform;
+};
+VSOut main( float2 pos : POSITION, float3 color : COLOR )
+{
+    VSOut vso;
+    vso.pos = mul(float4(pos.x, pos.y, 0.0f, 1.0f), transform);
+    vso.color = color;
+    return vso;
 }
